1b5811300fcd7f36c809adcd188d809161cd6f91
marvinax <nightvista@gmail.com>
2016-07-08 13:06:52 +0800

    reorganized directory structure

--- Assets/SaigoGameAssets/Bootstrap/Considering merge all bootstrap scenes.cs
+++ Assets/SaigoGameAssets/Bootstrap/Considering merge all bootstrap scenes.cs
@@ -1,13 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
-
-public class Debugger : MonoBehaviour {
+public class Consideringmergeallbootstrapscenes : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		gameObject.GetComponent<Text>().text = Screen.dpi.ToString() + " DPI";
+	
 	}
 	
 	// Update is called once per frame
